7a2054d61772306db601ab2927960224b9eb4fab
jachymneumiprogramovat <82580668+jachymneumiprogramovat@users.noreply.github.com>
2021-11-28 22:56:09 +0100

    les go tohleto jsem docela zničil
    pls nikdo se nekoukejte co jsem tady dělal před tím. Je to absolutní zmatek a anarchie

--- série 2/podivná brigáda/podivna-brigada.js
+++ série 2/podivná brigáda/podivna-brigada.js
@@ -1,46 +1,32 @@
 var fs = require("fs");
 const { off } = require("process");
-var input = fs.readFileSync("C:/Users/Běžný_člověk/Desktop/ksp2021/série 2/podivná brigáda/01 (1).in").toString()
+var input = fs.readFileSync("C:/Users/Běžný_člověk/Desktop/ksp2021/série 2/podivná brigáda/05.in").toString()
 input = input.split("\n")
 input.shift()
-let poradi = 0
+
+//true je kevin false je sára
+let Kcas = 0
+let Scas = 0
 let cas = 0
 
-console.log(input)
-function main(){
-    if(poradi>=input.length-1){
-        return cas
-    }
-cas += parseInt(input[poradi])
-poradi++
-console.log(cas)
-    if(input[poradi-1] != input[poradi]){
-        let rozdil = input[poradi]-input[poradi-1]
-        let pocet=1
-        for(;;){
-            rozdil = rozdil-input[poradi+1]
-            if(rozdil>=0){
-                pocet++
-            }else{
-                break
-            }
-        }
 
-        for(let i=0;i<pocet;i++){
-            BereJenJeden()
-        }
-        main()
-    } else{
-        poradi++
-        main()
+for(el of input){
+    if(Kcas<Scas){
+        Kcas+=parseInt(el)
+        
+    }else if(Kcas>Scas){
+        Scas+=parseInt(el)
+    }
+    else{
+        Kcas+=parseInt(el)
     }
 }
-
-function BereJenJeden(){
-    cas += parseInt(input[poradi])
-    poradi++
+if(Kcas>Scas){
+cas = Kcas
+}else{
+    cas = Scas
 }
+console.log(Kcas,Scas)
 
-main()
-
-fs.writeFileSync("ksp2021kps-vystup.txt",cas +"")
+ 
+fs.writeFileSync("ksp2021kps-vystup.txt",cas+"")
